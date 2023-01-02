0c174d2f3b2ad2bb39c9b151ce1090c3dd636112
Ariel Frischer <ariel.frischer@checkr.com>
2023-01-02 10:39:47 -0800

    add __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alias-gen",  # Replace with your package name
+    version="0.1",  # Replace with your package version
+    author="Ariel Frischer",  # Replace with your name
+    author_email="arielfrischer@gmail.com",  # Replace with your email
+    description="Generate bash/zsh/fish shell aliases automatically.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ariel-frischer/alias-gen",  # Replace with your package URL
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "alias",
+        "bash",
+        "zsh",
+        "fish",
+        "shell",
+        "cli",
+        "command-line",
+        "command",
+        "completion",
+        "abbr",
+        "generate",
+        "terminal",
+    ],
+)
