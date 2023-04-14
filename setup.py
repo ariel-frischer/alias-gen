c4e3bc9f92f170015b390cccd5bc9c7f80d033d9
Ariel Frischer <ariel.frischer@checkr.com>
2023-04-13 23:12:04 -0700

    Update version add wheel to build requirements. Cleanup some config stuff.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="alias-gen",  # Replace with your package name
-    version="0.1",  # Replace with your package version
-    author="Ariel Frischer",  # Replace with your name
-    author_email="arielfrischer@gmail.com",  # Replace with your email
+    name="alias-gen",
+    version="0.3",
+    author="Ariel Frischer",
+    author_email="arielfrischer@gmail.com",
     description="Generate bash, zsh, or fish shell aliases automatically.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/ariel-frischer/alias-gen",  # Replace with your package URL
+    url="https://gitlab.com/ariel-frischer/alias-gen",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
